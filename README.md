2c97cc8f91656d05d60254e76ba56e2399cf6e29
Justin Montgomery <montgomerycodepro@gmail.com>
2020-02-23 21:40:22 -0500

    doing some of the bonues items for my portfolio

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Updated-Portfolio
+
+Here is my Updated Portfolio. 
+
+Recently added all my work from the first part of the bootcamp.
+
---
+++ assets/script/script.js
@@ -0,0 +1,18 @@
+var gitHub = document.getElementById("github")
+var linkedIn = document.getElementById("linkedin")
+var twitter = document.getElementById("twitter")
+
+gitHub.addEventListener("click", function(){
+    window.open("https://github.com/JustinMonty20")  
+    // console.log("works")
+})
+
+linkedIn.addEventListener("click", function (){
+    window.open("https://www.linkedin.com/in/justin-montgomery-62a46a16a/")
+   
+})
+
+twitter.addEventListener("click", function(){
+    window.open("https://twitter.com/JustinMunty222")
+
+})
